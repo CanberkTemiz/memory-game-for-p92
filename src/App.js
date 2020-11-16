1c14283dc6c718d98ebaa354611abb81168d91af
Canberk Temiz <canberk.tmz@gmail.com>
2020-11-16 14:30:23 +0100

    created MemoryGame component. adjust login html

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { useEffect } from "react";
 import { useStore } from "./Store";
 
 import Login from "./Components/Login";
+import MemoryGame from "./Components/MemoryGame";
 import { useObserver } from "mobx-react";
 
 export default function App() {
   const store = useStore();
 
   return useObserver(() => {
-    return <div>{store.user.isLogged ? "" : <Login />}</div>;
+    return (
+      <div className="container">
+        {store.user.isLogged ? (
+          <div className="row">
+            <MemoryGame />
+          </div>
+        ) : (
+          <div className="row">
+            <Login />
+          </div>
+        )}
+      </div>
+    );
   });
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -14,7 +14,16 @@ export default function Login() {
 
   return (
     <div>
+      <h2>Rules</h2>
+      <ul>
+        <li>Click any card to see a hidden number</li>
+        <li>Find a card pair that shares same number</li>
+        <li>If card are not matching, they be flip back</li>
+        <li>Game ends when all cards are removed</li>
+      </ul>
+
       <form onSubmit={(e) => handleFormSubmit(e)}>
+        <h3> Set pair of card to start</h3>
         <input
           type="text"
           value={option}
